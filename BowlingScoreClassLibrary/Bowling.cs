2618694a72762b53555a711e04eb3b7b4aa0469e
rbpelenda <rbpelenda@gmail.com>
2021-03-22 23:57:47 +0100

    Inizion implementazione Class Bowling

--- BowlingScoreClassLibrary/Bowling.cs
+++ BowlingScoreClassLibrary/Bowling.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LancioClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,14 @@ namespace BowlingScoreClassLibrary
 
     public class Bowling : IBowling
     {
+        private ILancio[] _bow;
+
+        public Bowling(ILancio[] bow)
+        {
+
+            _bow = bow;
+
+        }
         public int Score()
         {
             throw new NotImplementedException();
