0097c035783cf986c0c3b2c5d87dc62d03706b64
rbpelenda <rbpelenda@gmail.com>
2021-03-23 00:10:37 +0100

    Aggiunto BowlingClassLibraryTest

---
+++ BowlingClassTests/BowlingClassTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingClassTests
+{
+    [TestFixture]
+    public class BowlingClassTest
+    {
+        [Test]
+        public void SumTest()
+        {
+
+        }
+    }
+}
