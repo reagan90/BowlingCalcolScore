98fbe7485996e4e2973141a857b4419b150d3204
rbpelenda <rbpelenda@gmail.com>
2021-03-22 23:53:39 +0100

    Fine implentazione Class Lancio

--- LancioClassLibrary/Lancio.cs
+++ LancioClassLibrary/Lancio.cs
@@ -15,7 +15,51 @@ namespace LancioClassLibrary
         void FirstSet(int n);
         void SecondSet(int n);
     }
-    public class Lancio
+    public class Lancio : ILancio
     {
+        public int _first { get; set; }
+        public int _second { get; set; }
+
+        public Lancio()
+        {
+            _first = 0;
+            _second = 0;
+        }
+
+        public Lancio(int first, int second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public int FirstGet()
+        {
+            return _first;
+        }
+
+        public void FirstSet(int n)
+        {
+            _first = n;
+        }
+
+        public bool IsSpare()
+        {
+            return (_first + _second == 10);
+        }
+
+        public bool IsTrike()
+        {
+            return _first == 10;
+        }
+
+        public int SecondGet()
+        {
+            return this._second;
+        }
+
+        public void SecondSet(int n)
+        {
+            this._second = n;
+        }
     }
 }
