a2fb4c408de0ad816190771eaa1162e8d203a3c2
rbpelenda <rbpelenda@gmail.com>
2021-03-22 23:44:50 +0100

    Aggiunto dell'interfaccia IBowling

---
+++ BowlingScoreClassLibrary/Bowling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScoreClassLibrary
+{
+    public interface IBowling
+    {
+        int Score();
+
+    }
+
+    public class Bowling : IBowling
+    {
+        public int Score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
