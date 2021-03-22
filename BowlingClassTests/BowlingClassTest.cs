a31bc141aa376116e9c8ef07f5418510f8733c0d
rbpelenda <rbpelenda@gmail.com>
2021-03-23 00:29:01 +0100

    Aggiunto TestScore

--- BowlingClassTests/BowlingClassTest.cs
+++ BowlingClassTests/BowlingClassTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using BowlingScoreClassLibrary;
+using LancioClassLibrary;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,41 @@ namespace BowlingClassTests
         [Test]
         public void SumTest()
         {
+            ILancio[] lancio = new ILancio[12];
+            for (int i = 0; i < lancio.Length; i++)
+                lancio[i] = new Lancio();
+            lancio[0].FirstSet(3);
+            lancio[0].SecondSet(4);
 
+            lancio[1].FirstSet(4);
+            lancio[1].SecondSet(6);
+
+            lancio[2].FirstSet(4);
+            lancio[2].SecondSet(3);
+
+            lancio[3].FirstSet(5);
+            lancio[3].SecondSet(4);
+
+            lancio[4].FirstSet(1);
+            lancio[4].SecondSet(0);
+
+            lancio[5].FirstSet(0);
+            lancio[5].SecondSet(7);
+
+            lancio[6].FirstSet(10);
+
+            lancio[7].FirstSet(4);
+            lancio[7].SecondSet(5);
+
+            lancio[8].FirstSet(6);
+            lancio[8].SecondSet(3);
+
+            lancio[9].FirstSet(3);
+            lancio[9].SecondSet(3);
+
+            IBowling bowling = new Bowling(lancio);
+
+            Assert.That(() => bowling.Score(), Is.EqualTo(88));
         }
     }
 }
